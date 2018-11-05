6816267c746c08e08161f390842fd67cf0ecbdbf
amirj <amir.jadidi@yahoo.com>
2018-11-05 13:35:47 +0000

    comment some part

--- spotlight/factorization/_components.py
+++ spotlight/factorization/_components.py
@@ -16,8 +16,9 @@ def _predict_process_ids(user_ids, item_ids, num_items, use_cuda):
     user_ids = torch.from_numpy(user_ids.reshape(-1, 1).astype(np.int64))
     item_ids = torch.from_numpy(item_ids.reshape(-1, 1).astype(np.int64))
 
-    if item_ids.size()[0] != user_ids.size(0):
-        user_ids = user_ids.expand(item_ids.size())
+    # if item_ids.size()[0] != user_ids.size(0):
+    #     user_ids = user_ids.expand(item_ids.size())
+    
 
     user_var = gpu(user_ids, use_cuda)
     item_var = gpu(item_ids, use_cuda)
